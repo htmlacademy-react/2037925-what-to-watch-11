364dc3eb21a6b79114250d2d81d178762baabd40
Ibrahim <danetov88@gmail.com>
2022-11-05 15:11:48 +0300

    Создал и связал компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Data = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  date: 2014
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title = {Data.title}
+      genre = {Data.genre}
+      date = {Data.date}
+    />
   </React.StrictMode>,
 );
